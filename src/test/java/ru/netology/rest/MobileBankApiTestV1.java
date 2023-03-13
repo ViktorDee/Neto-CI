c4132517e5fdc266b7fbabed781e477eb82dcb97
ViktorD <vdimitrenko96@gmail.com>
2023-03-13 17:43:53 +0700

    added chmod +x gradlew to .appveyor.yml

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,12 +1,11 @@
 package ru.netology.rest;
 
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+
 
 class MobileBankApiTestV1 {
     @Test
